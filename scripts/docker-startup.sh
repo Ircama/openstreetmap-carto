41e362b280609114e48db0f7d86a09c6c2217022
Ircama <amacri@tiscali.it>
2017-07-09 12:58:44 +0200

    Some small improvements to scripts/docker-startup.sh

--- scripts/docker-startup.sh
+++ scripts/docker-startup.sh
@@ -3,22 +3,25 @@
 # This script is used to start the import or kosmtik containers for the Docker development environment.
 # You can read details about that in DOCKER.md
 
+set -euo pipefail
+
 i=1
 MAXCOUNT=60
 echo "Waiting for PostgreSQL to be running"
-while [ $i -le $MAXCOUNT ]
+while [[ $i -le $MAXCOUNT ]]
 do
   pg_isready -q && echo "PostgreSQL running" && break
   sleep 2
   i=$((i+1))
 done
-test $i -gt $MAXCOUNT && echo "Timeout while waiting for PostgreSQL to be running"
+test $i -gt $MAXCOUNT && echo "---> Timeout while waiting for PostgreSQL to be running" && exit 1
 
 case "$1" in
 import)
   psql -c "SELECT 1 FROM pg_database WHERE datname = 'gis';" | grep -q 1 || createdb gis && \
   psql -d gis -c 'CREATE EXTENSION IF NOT EXISTS postgis;' && \
   psql -d gis -c 'CREATE EXTENSION IF NOT EXISTS hstore;' && \
+  test ! -f "$OSM2PGSQL_DATAFILE" && echo "---> Error. Missing file \"$OSM2PGSQL_DATAFILE\"" && exit 1
   osm2pgsql \
   --cache $OSM2PGSQL_CACHE \
   --number-processes $OSM2PGSQL_NUMPROC \
@@ -27,13 +30,13 @@ import)
   --database gis \
   --style openstreetmap-carto.style \
   --tag-transform-script openstreetmap-carto.lua \
-  $OSM2PGSQL_DATAFILE
+  "$OSM2PGSQL_DATAFILE"
   ;;
 kosmtik)
   python scripts/get-shapefiles.py -n
 
   mkdir -p .config
-  if [ ! -e ".kosmtik-config.yml" ]; then
+  if [[ ! -e ".kosmtik-config.yml" ]]; then
     touch .kosmtik-config.yml
     # this can be removed once https://github.com/kosmtik/kosmtik/issues/236 is resolved
     echo "plugins:" >> .kosmtik-config.yml
